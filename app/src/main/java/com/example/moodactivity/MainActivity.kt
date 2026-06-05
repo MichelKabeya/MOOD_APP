e43254c9ea5bcd09ede17387451af335041d0bb2
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-06-05 14:10:29 +0200

    declared all the variables

--- app/src/main/java/com/example/moodactivity/MainActivity.kt
+++ app/src/main/java/com/example/moodactivity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.moodactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        // DECLARATIONS
+        val getBtn = findViewById<Button>(R.id.getBtn)
+        getBtn.visibility = Button.INVISIBLE
+        val goBtn = findViewById<Button>(R.id.goBtn)
+        val resetBtn = findViewById<Button>(R.id.resetBtn2)
+        resetBtn.visibility = Button.INVISIBLE
+        val infoBtn = findViewById<Button>(R.id.infoBtn)
+        val inputText = findViewById<EditText>(R.id.inputText)
+        inputText.visibility = EditText.INVISIBLE
+        val activityText = findViewById<TextView>(R.id.activityText)
+        val promptText = findViewById<TextView>(R.id.prompText)
+        promptText.visibility = TextView.INVISIBLE
+        val titleText = findViewById<TextView>(R.id.titleText)
+
+       
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

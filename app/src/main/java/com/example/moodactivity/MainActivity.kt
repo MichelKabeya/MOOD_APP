c91ff51c47c3be9fd6b06926a408f720794031ab
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-06-05 14:11:07 +0200

    declared arrays of moods and activities

--- app/src/main/java/com/example/moodactivity/MainActivity.kt
+++ app/src/main/java/com/example/moodactivity/MainActivity.kt
@@ -28,7 +28,50 @@ class MainActivity : AppCompatActivity() {
         promptText.visibility = TextView.INVISIBLE
         val titleText = findViewById<TextView>(R.id.titleText)
 
-       
+        // ARRAY OF MOOD
+        val moods = arrayOf(
+            "Happy",
+            "Sad",
+            "Tired",
+            "Anxious",
+            "Angry",
+            "Relaxed",
+            "Excited",
+            "Bored",
+            "Stressed",
+            "Curious"
+        )
+        // ARRAY OF ACTIVITIES
+
+        val activities = arrayOf(
+            "Call or message a friend.",
+            "Take a 20-minute walk outdoors.",
+            "Do 10 minutes of stretching.",
+            "Practice a breathing exercise.",
+            "Go for a jog or workout.",
+            "Read a book.",
+            "Plan a new project.",
+            "Solve a puzzle or play chess.",
+            "Talk with a trusted friend.",
+            "Watch an educational video."
+        )
+
+        // CODE LINES / EXECUTION
+        // GO button takes away all unnecessary elements on the screen when clicked
+        goBtn.setOnClickListener {
+            titleText.visibility = TextView.INVISIBLE
+            goBtn.visibility = Button.INVISIBLE
+            getBtn.visibility = Button.VISIBLE
+            inputText.visibility = EditText.VISIBLE
+            promptText.visibility = TextView.VISIBLE
+            activityText.text = ""
+        }
+
+        getBtn.setOnClickListener {
+            val mood = inputText.text.toString()
+            val index = moods.indexOf(mood)
+            if ()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
